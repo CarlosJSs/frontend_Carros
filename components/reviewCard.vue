b95259ab3aed10566a3b5220ec4129a52b7def42
Carlos Eduardo <carlos.0577@outlook.com>
2024-11-13 11:13:47 -0600

    primer diseño de carCard

---
+++ components/reviewCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Reseña de los clientes
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/cliente/detailCar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row justify="center" align="center" class="index-background">
+    <carProfile />
+  </v-row>
+</template>
+
+<script>
+import carProfile from '@/components/carProfile.vue'
+
+export default {
+  name: 'DetailCarPage',
+  components: {
+    carProfile
+  },
+  layout: 'clienteLayout',
+  middleware: 'detect-push'
+}
+</script>
+
+<style scoped>
+.index-background {
+  background-color: #8e9096;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
