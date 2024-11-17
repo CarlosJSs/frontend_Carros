039d55bfcf15a4bc198069b1e36b20bd2650ad3c
Carlos Eduardo <carlos.0577@outlook.com>
2024-11-16 19:36:21 -0600

    inicio del AddForm para carros

---
+++ components/tableCars.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Hola tabla
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
---
+++ pages/admin/addCar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row justify="center" align="start" class="index-background">
+    <addCarForm />
+    <tableCars />
+  </v-row>
+</template>
+
+<script>
+import addCarForm from '@/components/addCarForm.vue'
+import tableCars from '@/components/tableCars.vue'
+
+export default {
+  name: 'AddCarPage',
+  components: {
+    addCarForm,
+    tableCars
+  },
+  layout: 'adminLayout',
+  middleware: 'detect-push'
+}
+</script>
+
+<style scoped>
+.index-background {
+  background-color: #f6f7f9;
+  width: 100vw;
+  height: auto;
+  padding-top: 6vh;
+  padding-bottom: 6vh;
+}
+</style>
