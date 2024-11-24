a8dfa47b7dc38b20fcbf27a7b28c86a040e6a743
Carlos Eduardo <carlos.0577@outlook.com>
2024-11-24 11:17:35 -0600

    addForm component

--- components/addCarForm.vue
+++ components/addCarForm.vue
@@ -1,6 +1,46 @@
 <template>
-  <div>
-    Hola addForm
+  <div class="billingInfoContainer">
+    <div class="headCard">
+      <div class="titleCard">
+        Add a new Car
+      </div>
+      <div class="descCard">
+        <div class="descInfo">
+          Please enter your billing info
+        </div>
+        <div class="stepCard">
+          Step 1 of 4
+        </div>
+      </div>
+    </div>
+    <div class="rowInputs">
+      <div class="labelAndInput">
+        <div class="labelInput">
+          Name
+        </div>
+        <input type="text" placeholder="Your name" class="inputInfo">
+      </div>
+      <div class="labelAndInput">
+        <div class="labelInput">
+          Phone Number
+        </div>
+        <input type="tel" placeholder="Phone number" class="inputInfo">
+      </div>
+    </div>
+    <div class="rowInputs">
+      <div class="labelAndInput">
+        <div class="labelInput">
+          Address
+        </div>
+        <input type="text" placeholder="Address" class="inputInfo">
+      </div>
+      <div class="labelAndInput">
+        <div class="labelInput">
+          Town / City
+        </div>
+        <input type="text" placeholder="Town or city" class="inputInfo">
+      </div>
+    </div>
   </div>
 </template>
 
